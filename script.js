d2a251ed9f92dd11814449c0917168ba754a6390
Harshwardhan patil <harshwardhanpatil340@gmail.com>
2024-01-11 23:22:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// var crsr = document.querySelector("#cursor");
+// var blur = document.querySelector("#cursor-blur");
+
+// document.addEventListener("mousemove", function (dets) {
+//   crsr.style.left = dets.x + "px";
+//   crsr.style.top = dets.y + "px";
+//   blur.style.left = dets.x - 250 + "px";
+//   blur.style.top = dets.y - 250 + "px";
+// });
+
+
+burger = document.querySelector(".burger")
+
+
+
+burger.addEventListener("click",()=>{
+    
+})
