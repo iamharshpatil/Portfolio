c9df4367ff6184497a8ffab7144333caa2891c14
Harshwardhan patil <harshwardhanpatil340@gmail.com>
2024-01-14 23:38:40 +0530

    replace all code again

--- script.js
+++ script.js
@@ -8,13 +8,27 @@
 //   blur.style.top = dets.y - 250 + "px";
 // });
 
-alert("This site is in under development and it is Not developed for an Mobile yet :)");
-
-
-burger = document.querySelector(".burger")
+// alert("This site is in under development and it is Not developed for an Mobile yet :)");
 
 
 
-burger.addEventListener("click",()=>{
-    
+var menu = document.querySelector("h4")
+var blank = document.querySelector(".blank")
+var icon = document.querySelector("h4")
+
+
+var flag = 0
+
+
+menu.addEventListener("click",()=>{
+
+    if(flag == 0){
+     blank.style.top = 0 ;
+     icon.innerHTML = '"<i class="ri-close-line"></i>"'
+     flag = 1
+    }else{
+        blank.style.top = "-100%" ;
+        icon.innerHTML = '"<i class="ri-menu-3-line"></i></i>"'
+        flag = 0 
+    }
 })
