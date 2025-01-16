3dbe05ebbba72a45b8580751def59ab024f0da40
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-01-16 19:35:53 +0530

    initialize project with React, Vite, and Tailwind CSS; add essential components and assets

---
+++ src/constants/index.js
@@ -0,0 +1,313 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Work",
+    href: "#work",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: "Emily Johnson",
+    position: "Marketing Director at GreenLeaf",
+    img: "assets/review1.png",
+    review:
+      "Working with Harshwardhan was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+  },
+  {
+    id: 2,
+    name: "Mark Rogers",
+    position: "Founder of TechGear Shop",
+    img: "assets/review2.png",
+    review:
+      "Harshwardhan’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.",
+  },
+  {
+    id: 3,
+    name: "John Dohsas",
+    position: "Project Manager at UrbanTech ",
+    img: "assets/review3.png",
+    review:
+      "I can’t say enough good things about Harshwardhan. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+  },
+  {
+    id: 4,
+    name: "Ether Smith",
+    position: "CEO of BrightStar Enterprises",
+    img: "assets/review4.png",
+    review:
+      "Harshwardhan was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.",
+  },
+];
+
+export const myProjects = [
+  {
+    title: "PrimeMax - Stream Your Favorites",
+    desc: "PrimeMax is a dynamic movie-streaming website offering a seamless viewing experience for cinema enthusiasts. With an extensive library of films across genres, PrimeMax ensures endless entertainment at your fingertips.",
+    subdesc:
+      "Built with React.js, PrimeMax combines a sleek interface with robust functionality to deliver high-quality streaming. From blockbusters to hidden gems, PrimeMax is your ultimate destination for movies.",
+    href: "https://movie-app-uxgy.onrender.com/",
+    texture: "/textures/project/project1.mp4",
+    logo: "/assets/project-logo1.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: 'Significo Clone - An Immersive Web Experience',
+desc: 'This project replicates the immersive design and interactive web experience of Significo. It showcases advanced scrolling effects and dynamic animations to create an engaging user experience.',
+subdesc: 'Developed using HTML5, CSS, Locomotive Scroll, ScrollTrigger, and GSAP, the Significo clone demonstrates expertise in modern frontend technologies and a focus on detail-oriented web design.',
+href: "https://iamharshpatil.github.io/Significo/",
+    texture: "/textures/project/project2.mp4",
+    logo: "/assets/project-logo2.jpeg",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: 'FileFusion-Drive - Your Ultimate Cloud Storage Solution',
+desc: 'FileFusion-Drive is a robust cloud storage platform that offers secure and efficient file management. With features like seamless file uploads, sharing, and real-time access across devices, it ensures your data is always at your fingertips.',
+subdesc: 'Built with NodeJS, FileFusion-Drive combines advanced cloud technology with user-friendly design, making it the ideal solution for personal and professional storage needs.'
+,
+    href: "https://filefusion-drive.onrender.com/",
+    texture: "/textures/project/project3.mp4",
+    logo: "/assets/project-logo3.png",
+    logoStyle: {
+      backgroundColor: "#60f5a1",
+      background:
+        "linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(208, 213, 221, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(35, 131, 96, 0.3)",
+    },
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "Horizon - Online Banking Platform",
+    desc: "Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.",
+    subdesc:
+      "Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.",
+    href: "",
+    texture: "/textures/project/project4.mp4",
+    logo: "/assets/project-logo4.png",
+    logoStyle: {
+      backgroundColor: "#0E1F38",
+      border: "0.2px solid #0E2D58",
+      boxShadow: "0px 0px 60px 0px #2F67B64D",
+    },
+    spotlight: "/assets/spotlight4.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "PrimeMax - Stream Your Favorites",
+    desc: "PrimeMax is a dynamic movie-streaming website offering a seamless viewing experience for cinema enthusiasts. With an extensive library of films across genres, PrimeMax ensures endless entertainment at your fingertips",
+    subdesc:
+      "Built with React.JS, PrimeMax combines a sleek interface with robust functionality to deliver high-quality streaming. From blockbusters to hidden gems, PrimeMax is your ultimate destination for movies.",
+    href: "",
+    texture: "/textures/project/project5.mp4",
+    logo: "/assets/project-logo5.png",
+    logoStyle: {
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
+    },
+    spotlight: "/assets/spotlight5.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall
+      ? [4, -5, 0]
+      : isMobile
+        ? [5, -5, 0]
+        : isTablet
+          ? [5, -5, 0]
+          : [9, -5.5, 0],
+    reactLogoPosition: isSmall
+      ? [3, 4, 0]
+      : isMobile
+        ? [5, 4, 0]
+        : isTablet
+          ? [5, 4, 0]
+          : [12, 3, 0],
+    ringPosition: isSmall
+      ? [-5, 7, 0]
+      : isMobile
+        ? [-10, 10, 0]
+        : isTablet
+          ? [-12, 10, 0]
+          : [-24, 10, 0],
+    targetPosition: isSmall
+      ? [-5, -10, -10]
+      : isMobile
+        ? [-9, -10, -10]
+        : isTablet
+          ? [-11, -7, -10]
+          : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: "Open to Work",
+    pos: "Freelance Web Developer",
+    duration: "2022 - Present",
+    title:
+      "As a freelance web developer, I am actively seeking new opportunities to collaborate on innovative projects. I specialize in full-stack web development, using modern frameworks and tools to create scalable and user-friendly websites.",
+    icon: "/assets/framer.svg",
+    animation: "victory",
+  },
+  {
+    id: 2,
+    name: "FunnelByte",
+    pos: "Data Research Executive",
+    duration: "Nov 2021 - Jul 2022",
+    title:
+      "At Funnelbyte we aspire to provide the most personalized kind of data solutions for every customer. We are the torchbearers in providing data-driven outcomes. Believing in excellence and adaptability, we help keep your data accurate, updated, as well as validated.",
+    icon: "/assets/Funnelbyte.png",
+    animation: "clapping",
+  },
+  {
+    id: 3,
+    name: "FreeLancer",
+    pos: "Web Developer",
+    duration: "Jun 2021 - Aug 2021",
+    title:"From concept to deployment, I specialize in creating user-friendly, responsive websites that drive engagement and enhance user experience.",
+    icon: "/assets/upwork.jpg",
+    animation: "salute",
+  },
+];
