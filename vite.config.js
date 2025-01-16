991c507f36544158fe4707f78a5e8d996d98b508
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-01-16 20:08:24 +0530

    add vite-plugin-compression for file compression in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCompression({
+      // Compression settings (optional)
+      algorithm: 'gzip',  // Choose 'gzip' or 'brotliCompress'
+      threshold: 10240,   // Only compress files larger than 10kB
+      ext: '.gz',         // Compressed file extension (you can use '.br' for Brotli)
+    }),
+  ],
+});
